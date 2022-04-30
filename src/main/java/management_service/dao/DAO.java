7826e78ab2224c72855cc7a757504fdfe1f170d8
rilialov <ilialovrustem@gmail.com>
2022-04-30 15:25:04 +0300

    Added flyway and initial script

---
+++ src/main/java/management_service/dao/DAO.java
@@ -0,0 +1,13 @@
+package management_service.dao;
+
+import java.util.List;
+
+public interface DAO<T> {
+    List<T> getAll();
+
+    T create(T t);
+
+    void update(T t);
+
+    void delete(T t);
+}
